50ff5c2e2dcf082b18ea8cc01e8ef4abe5022023
hodn <hoang.doan@rocketmail.com>
2021-12-19 09:08:13 +0100

    Updates

---
+++ precache-manifest.6cec53e96f710e99f1c79aefe2ea5f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8edc4359e55d1f1e0ed4",
+    "url": "/wordathon/static/css/main.4b2358a0.chunk.css"
+  },
+  {
+    "revision": "8edc4359e55d1f1e0ed4",
+    "url": "/wordathon/static/js/main.654cb9a2.chunk.js"
+  },
+  {
+    "revision": "a2ba4f47e34b97520e4a",
+    "url": "/wordathon/static/js/runtime~main.e8a0a593.js"
+  },
+  {
+    "revision": "57edd54349807623b4cb",
+    "url": "/wordathon/static/js/2.a86d8f14.chunk.js"
+  },
+  {
+    "revision": "11ca0c5a0eb77f9c95c52f6bdae952a3",
+    "url": "/wordathon/index.html"
+  }
+];
