14e869aedd0361ed92c30e1fa874d5c090cebc70
hodn <hoang.doan@rocketmail.com>
2021-12-25 22:07:06 +0100

    share test

--- src/components/InviteLinkCard.js
+++ src/components/InviteLinkCard.js
@@ -7,8 +7,14 @@ import Snackbar from '@material-ui/core/Snackbar';
 
 
 export default function InviteLinkCard(props) {
-    const inviteLink = 'https://' + window.location.href.split('/')[2] + '/' + window.location.href.split('/')[3] +"/join/" + props.room.ID;
+    const inviteLink = 'https://' + window.location.href.split('/')[2] + '/' + window.location.href.split('/')[3] + "/join/" + props.room.ID;
     const [snackOpen, openSnack] = React.useState(false);
+    const room = props.room;
+    const dataShare = {
+        title: 'Wordathon',
+        text: room.players[room.ownerID].name + ' challenges you to a game!',
+        url: inviteLink
+      }
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(inviteLink);
@@ -19,10 +25,18 @@ export default function InviteLinkCard(props) {
         }, 2000);
     }
 
+    const shareLink = async() => {
+        try {
+            await navigator.share(dataShare)
+          } catch(err) {
+              console.log(err);
+          }
+    }
+
     return (
         <div>
 
-            <Typography variant='h6' style={{marginBottom: 10}}> Invite friends</Typography>
+            <Typography variant='h6' style={{ marginBottom: 10 }}> Invite friends</Typography>
 
 
             <Grid
@@ -48,15 +62,26 @@ export default function InviteLinkCard(props) {
                     </Typography>
                 </Grid>
 
-                <Grid item> 
-                    <Button
+                <Grid item>
+                   <Button
+                        onClick={shareLink}
+                        variant="contained"
+                        color="primary"
+                        style={{ marginLeft: 'auto' }}
+                    >
+                        Share invite
+                    </Button>
+
+                    <br/>
+
+                    {!navigator.canShare &&<Button
                         onClick={copyToClipboard}
                         variant="contained"
                         color="primary"
                         style={{ marginLeft: 'auto' }}
                     >
                         Copy link
-                    </Button>
+                    </Button>}
                 </Grid>
 
             </Grid>
