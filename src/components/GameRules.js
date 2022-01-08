b9b7822ee6bd3e4d6a27dbb7d1fe155e62204c2a
hodn <hoang.doan@rocketmail.com>
2022-01-08 13:12:16 +0100

    new rules

--- src/components/GameRules.js
+++ src/components/GameRules.js
@@ -19,7 +19,7 @@ export default function GameRules() {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Wordathon is a scrabble-like fun online multiplayer game. Create a game room and invite your friends on any device or platform.
+                        <strong>Wordathon</strong> is a scrabble-like fun online multiplayer game. Create a game room and invite your friends on any device or platform.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
@@ -37,9 +37,9 @@ export default function GameRules() {
                         <p>The main goal of the game is to <strong>form nouns</strong> from the provided letters. The more words and the longer words, the better!</p>
                         <h3>For each existing noun you receive:</h3>
                         <ul>
-                            <li><span style={{color: '#ffffff', backgroundColor: '#ff9900'}}>20 points</span> <strong>X</strong> the letter count</li>
-                            <li><span style={{color: '#ffffff', backgroundColor: '#339966'}}>extra 100 points</span> for using it as a first player in the round</li>
-                            <li>extra 300 points for <em>5 or more</em> lettered word</li>
+                            <li><span style={{ color: '#ffffff', backgroundColor: '#ff9900' }}>20 points</span> <strong>X</strong> the letter count</li>
+                            <li><span style={{ color: '#ffffff', backgroundColor: '#339966' }}>extra 50 points</span> for using it as a first player in the round</li>
+                            <li><span style={{ color: '#ffffff', backgroundColor: 'grey' }}>extra 300 points</span> <em>5 or more</em> lettered word</li>
                         </ul>
                         <p>Enjoy!</p>
                     </Typography>
